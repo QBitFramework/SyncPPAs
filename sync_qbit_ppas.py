324d605f6105d5399bbfc6612c44bf42ba5aa842
Sergei Svistunov <sergei-svistunov@yandex.ru>
2013-09-03 17:15:26 +0400

    Fixed bug with binaries lists

--- sync_qbit_ppas.py
+++ sync_qbit_ppas.py
@@ -8,13 +8,13 @@ launchpad = Launchpad.login_with("QBit's PPAs synchronizer", 'production')
 team = launchpad.people["qbit-perl"]
 
 source_ppa = team.getPPAByName(name=SOURCE_PPA)
-source_packages = source_ppa.getPublishedSources();
+source_packages = source_ppa.getPublishedSources()
 
 for dest_ppa_name in DEST_PPAS:
     dest_ppa = team.getPPAByName(name=dest_ppa_name)
-    dest_packages = dest_ppa.getPublishedSources();
+    dest_packages = dest_ppa.getPublishedBinaries();
 
-    dest_packages_dict = dict([(p.source_package_name, p.source_package_version) for p in dest_packages])
+    dest_packages_dict = dict([(p.binary_package_name, p.binary_package_version) for p in sorted(dest_packages, key=lambda pkg: (pkg.binary_package_name, pkg.date_published))])
 
     for source_package in source_packages:
         if source_package.status <> u'Published':
@@ -22,12 +22,12 @@ for dest_ppa_name in DEST_PPAS:
 
         need_sync = False
         if not source_package.source_package_name in dest_packages_dict:
-            print "Package '%s (%s)' does not exists in '%s'," % (source_package.source_package_name, dest_packages_dict[source_package.source_package_name], dest_ppa_name),
+            print "Package '%s (%s)' does not exists in '%s'," % (source_package.source_package_name, source_package.source_package_version, dest_ppa_name),
             need_sync = True
-
-        if source_package.source_package_version > dest_packages_dict[source_package.source_package_name]:
-            print "Package '%s (%s)' is outdated in '%s'," % (source_package.source_package_name, dest_packages_dict[source_package.source_package_name], dest_ppa_name),
-            need_sync = True
+        else:
+            if source_package.source_package_version > dest_packages_dict[source_package.source_package_name]:
+                print "Package '%s (%s)' is outdated in '%s'," % (source_package.source_package_name, dest_packages_dict[source_package.source_package_name], dest_ppa_name),
+                need_sync = True
 
         if need_sync:
             print "synchronizing to version %s" % (source_package.source_package_version)
